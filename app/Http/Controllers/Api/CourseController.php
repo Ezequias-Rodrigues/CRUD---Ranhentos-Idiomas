a0f252878f7a6c6ae734cf3fedc0174f4fcd558d
Ezequias Rodrigues <7608708+Ezequias-Rodrigues@users.noreply.github.com>
2026-04-05 18:08:13 -0300

    Course controller

--- app/Http/Controllers/Api/CourseController.php
+++ app/Http/Controllers/Api/CourseController.php
@@ -3,47 +3,94 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Course;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 
 class CourseController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display a listing of the courses.
      */
     public function index()
     {
-        //
+       $courses = Course::all();
+       return response()->json($courses);
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Store a newly created course in storage.
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'duration' => 'nullable|integer|min:1',
+            'price' => 'required|required|numeric|between:0,999999999.99',
+            'max_students' => 'nullable|integer|min:1',
+
+        ],[ //Aqui em baixo são as mensagens de erro para que o usuario saiba oq ele fez de errado caso não valide
+            'name.required' => 'O nome do curso é obrigatório',
+            'duration.numeric' => 'A duração do curso deve ser um número válido',
+            'duration.min' => 'O curso deve ter duração igual ou superior a 1 semestre',
+            'price.required' => 'O preço do curso é obrigatório',
+            'price.numeric' => 'O preço deve ser um número válido',
+            'price.min' => 'O preço não pode ser negativo',
+            'max_students.min' => 'O número máximo de alunos deve ser pelo menos 1',
+        ]);
+        $course = Course::create($validated);
+        return response()->json($course, 201);
     }
 
     /**
-     * Display the specified resource.
+     * Display the specified course.
      */
     public function show(string $id)
     {
-        //
+        return response()->json(Course::findOrFail($id)); //Retorna 404 caso não haja o curso
     }
 
     /**
-     * Update the specified resource in storage.
+     * Update the specified course in storage.
      */
     public function update(Request $request, string $id)
     {
-        //
+        $validated = $request->validate([
+            'name' => 'sometimes|string|max:255',
+            'description' => 'nullable|string',
+            'duration' => 'nullable|integer',
+            'price' => 'sometimes|required|numeric|between:0,999999999.99',
+            'max_students' => 'nullable|integer|min:1',
+
+        ],[
+            'duration.numeric' => 'A duração do curso deve ser um número válido',
+            'duration.min' => 'O curso deve ter duração igual ou superior a 1 semestre',
+            'price.numeric' => 'O preço deve ser um número válido',
+            'price.min' => 'O preço não pode ser negativo',
+            'max_students.min' => 'O número máximo de alunos deve ser pelo menos 1',
+        ]);
+       // try
+        //{//Meu primeiro impulso foi tentar usar um try/catch aqui, mas ao pesquisar, vi que o findOrFail manda 404 caso não encontre o curso automaticamente
+        $course = Course::findOrFail($id); //Procura o curso por ID no DB
+        $course->update($validated);
+        return response()->json($course);
+        //}
+       // catch (ModelNotFoundException $e)
+        //{
+            //return response()->json([$e], 404);
+        //}
+
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Remove the specified course from storage.
      */
     public function destroy(string $id)
     {
-        //
+            $course = Course::findOrFail($id);
+            $course->delete();
+            return response()->json(null, 204);
+
     }
 }
--- app/Http/Controllers/Api/StudentController.php
+++ app/Http/Controllers/Api/StudentController.php
@@ -10,39 +10,39 @@ class StudentController extends Controller
 {
     public function index()
     {
-        $students = Student::all();
-        return response()->json($students);
+        $students = Student::all(); //básicamente um SELECT * FROM students
+        return response()->json($students); //Retorna um json com uma collection de todos os estudantes
     }
-    
+
     public function store(Request $request)
     {
-        $validated = $request->validate([
-            'name' => 'required|string|max:255',
+        $validated = $request->validate([ //Valida os dados, caso eles não sigam as regras que estão depois do "=>", retorna o erro 422 ou redireciona com erros
+            'name' => 'required|string|max:255',//Caso valide certinho, uma array com os campos definidos aqui é retornada
             'email' => 'required|email|unique:students',
-            'phone' => 'nullable|string'
+            'phone' => 'nullable|string' //Lembro que a gente conversou sobre String vs Int para guardar numeros de telefone, acredito que string se aplica aqui por ser uma demostração
         ]);
-        
-        $student = Student::create($validated);
-        return response()->json($student, 201);
+
+        $student = Student::create($validated); //Isso insere os dados na tabela, e retorna o ID
+        return response()->json($student, 201);//HTML status Created, ou seja, tudo certinho
     }
-    
+
     public function show(Student $student)
     {
-        return response()->json($student);
+        return response()->json($student); //Exibe os dados de Student em JSON
     }
-    
+
     public function update(Request $request, Student $student)
     {
-        $validated = $request->validate([
+        $validated = $request->validate([ //Validação normal
             'name' => 'sometimes|string|max:255',
             'email' => 'sometimes|email|unique:students,email,' . $student->id,
             'phone' => 'nullable|string'
         ]);
-        
-        $student->update($validated);
-        return response()->json($student);
+
+        $student->update($validated); //Atualiza os campos retornando True or False, porém aqui o valor está sendo descartado
+        return response()->json($student); //Exibe os dados em JSON
     }
-    
+
     public function destroy(Student $student)
     {
         $student->delete();
