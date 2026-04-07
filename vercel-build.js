0a485b43bae5e4fda640f96f34128dc3794103fc
Ezequias Rodrigues <7608708+Ezequias-Rodrigues@users.noreply.github.com>
2026-04-07 16:21:25 -0300

    Update vercel-build.js

--- vercel-build.js
+++ vercel-build.js
@@ -1,6 +1,6 @@
-const { execSync } = require('child_process');
-const fs = require('fs');
-const path = require('path');
+const { execSync } = import('child_process');
+const fs = import('fs');
+const path = import('path');
 
 
 
