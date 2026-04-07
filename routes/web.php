e1bb7d11c1e84dbea7c6b7d340ecbddcd00bce35
Ezequias Rodrigues <7608708+Ezequias-Rodrigues@users.noreply.github.com>
2026-04-07 15:24:25 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'message' => 'Ranhentos Idiomas API',
+        'version' => '1.0',
+        'frontend' => 'https://crud-ranhentos-idiomas.vercel.app/'
+    ]);
 });
 Route::get('/debug-db', function() {
     try {
